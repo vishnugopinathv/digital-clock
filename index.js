441acc50056dde8282af0264963fc4e3a70cc273
vishnugopinathv <vishnugopinathv@gmail.com>
2022-04-09 17:01:29 +0530

    dynamic change in pictures and text

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const clock=()=>{
 let time= new Date();
-let amorpm="PM";
+let amorpm="";
 let hour=time.getHours();
 let minute=time.getMinutes();
 let second=time.getSeconds();
@@ -9,12 +9,8 @@ let hours=document.getElementsByClassName("hour");
 let minutes=document.getElementsByClassName("minute");
 let seconds=document.getElementsByClassName("second");
 let am_pm=document.getElementsByClassName("am_pm");
-let timing=document.getElementsByClassName("timing");
-timing[0].innerText=document.getElementById("wakeup").value;
-timing[1].innerText=document.getElementById("lunch").value;
-timing[2].innerText=document.getElementById("nap").value;
-timing[3].innerText=document.getElementById("night").value;
-if(time.getHours()>12){
+
+if(hour>12){
     hour=hour-12;
     amorpm="PM";
 }
@@ -31,11 +27,52 @@ if(minute<10){
 if(second<10){
     second="0"+second;
 }
+if((hour>=8 && hour<12) && (amorpm==="AM")){
+    let greeting=document.getElementById("goodmorning");
+    let message=document.getElementById("greeting");
+    let image=document.getElementById("greeting-image");
+    image.src="./morning.svg";
+    message.innerText="GRAB SOME HEALTHY BREAKFAST!!!"
+    greeting.innerText="GOOD MORNING!! WAKEUP!!";
+}
+if(((hour>=12)|| (hour<4)) && (amorpm==="PM")){
+    let greeting=document.getElementById("goodmorning");
+    let message=document.getElementById("greeting");
+    let image=document.getElementById("greeting-image");
+    image.src="./lunch.svg";
+    message.innerText="LET'S HAVE SOME LUNCH !!"
+    greeting.innerText="GOOD AFTERNOON !! TAKE SOME SLEEP";
+}
+if((hour>=4 && hour<8) && (amorpm==="PM")){
+    let greeting=document.getElementById("goodmorning");
+    let message=document.getElementById("greeting");
+    let image=document.getElementById("greeting-image");
+    image.src="./evening.png";
+    message.innerText="STOP YAWNING, GET SOME TEA.. ITS JUST EVENING!"
+    greeting.innerText="GOOD EVENING!!";
+}
+if((hour>=8 && hour<12) && (amorpm==="PM")){
+    let greeting=document.getElementById("goodmorning");
+    let message=document.getElementById("greeting");
+    let image=document.getElementById("greeting-image");
+    image.src="./night.png";
+    message.innerText="CLOSE YOUR EYES AND GO TO SLEEP"
+    greeting.innerText="GOOD NIGHT!!";
+}
+
+
+
 hours[0].innerText=hour;
 minutes[0].innerText=minute;
 seconds[0].innerText=second;
 am_pm[0].innerText=amorpm;
 }
+const setAlarm=()=>{
+    let timing=document.getElementsByClassName("timing");
+    timing[0].innerText=document.getElementById("wakeup").value;
+    timing[1].innerText=document.getElementById("lunch").value;
+    timing[2].innerText=document.getElementById("nap").value;
+    timing[3].innerText=document.getElementById("night").value;
+}
 clock();
-
 setInterval(clock,1000);
